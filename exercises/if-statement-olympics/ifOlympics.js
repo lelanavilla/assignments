977bd5c587da811fb216d5a9362f85a33a8c5250
lelanavilla <cfsfresno@gmail.com>
2018-03-02 22:45:01 -0700

    submitting ifolympics exercise

--- exercises/if-statement-olympics/ifOlympics.js
+++ exercises/if-statement-olympics/ifOlympics.js
@@ -13,9 +13,9 @@ name : "Sam",
 age: 14
 }
 if(person.age >= 18 && person.name === "B" ){
-    console.log("is allowed to go to the movie"); 
+    console.log( person.name + " is allowed to go to the movie"); 
 }else {
-    console.log("is not allowed to go to the movie");
+    console.log(person.name + " is not allowed to go to the movie");
 };
 
 if ( 1 === "1"){
@@ -31,12 +31,30 @@ if((1 >= 2 && 2 === 4) || (3 * 4 ) > 10 && (5 + 10) > 10) {
 };
 
 
-if (typeof dog ==="string"){
-     console.log(string);
+if (typeof "dog" ==="string"){
+     console.log("string");
 
 
 };
-if (typeof true === Boolean){
-console.log(Boolean);
-};
-    
+
+console.log(typeof true);
+ var v="";   
+if(v === "defined" ){
+console.log("defined");
+}else {
+    console.log("undefined");
+}
+
+var letters =("a,b,c");
+var numbers = 2
+if ( letters < numbers){
+    console.log(letter);
+}else{
+    console.log("they arr different types silly");
+}
+var myNum  = 10;
+if(myNum % 2 === 0){
+    console.log("even");
+}else if ( myNum % 2 === 1){
+    console.log("odd");
+}
---
+++ exercises/noWayA/noWayA.js
@@ -0,0 +1,5 @@
+var removeA = function(str) {
+return str.indexOf().slice(a);
+
+}
+removeA();
