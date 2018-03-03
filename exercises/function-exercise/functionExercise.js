6644d3149b51f19c90ef26873eca37371302e67f
lelanavilla <cfsfresno@gmail.com>
2018-03-02 23:12:42 -0700

    submitting functionExercise

--- exercises/function-exercise/functionExercise.js
+++ exercises/function-exercise/functionExercise.js
@@ -1,49 +1,50 @@
-function sum(a,b){
+function sum(a, b) {
     return a + b;
 
 }
-sum(2,7);
+sum(2, 7);
 
-function theLargestNum(){
-  var arr=[1,2,3] ; 
-var largest = arr.sort().pop();
-console.log(largest);
+function theLargestNum() {
+    var arr = [1, 2, 3];
+    var largest = arr.sort().pop();
+    console.log(largest);
 }
 theLargestNum();
 
-function oddOrEven(num){
-    if(num % 2 === 0){
-    console.log( num + " is even.");
-    }else if( num % 2  === 1){
-    console.log (" is odd");
-}
+function oddOrEven(num) {
+    if (num % 2 === 0) {
+        console.log(num + " is even.");
+    } else if (num % 2 === 1) {
+        console.log(" is odd");
+    }
 }
 oddOrEven(4);
 
 
-function myStr(str){
-    if(str.length < 20){
-        console.log(str +  str);
-    }else if( str.length > 20){
+function myStr(str) {
+    if (str.length < 20) {
+        console.log(str + str);
+    } else if (str.length > 20) {
         console.log(str.length / 2);
     }
 }
 
 myStr("hi,good day");
 
-var fibonacci_series = function (n) 
-{
-  if (n===1) 
-  {
-    return [0, 1];
-  } 
-  else 
-  {
-    var s = fibonacci_series(n - 1);
-    s.push(s[s.length - 1] + s[s.length - 2]);
-    return s;
-  }
+var fibonacci_series = function (n) {
+    if (n === 1) {
+        return [0, 1];
+    } else {
+        var s = fibonacci_series(n - 1);
+        s.push(s[s.length - 1] + s[s.length - 2]);
+        return s;
+    }
 };
 
- console.log(fibonacci_series(8));
-  
+console.log(fibonacci_series(8));
+var mySeq = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+function getSum(total, num) {
+    total + num;
+    mySeq.reduce(mySum);
+}
