73c71bfc2828e59cda22992e446050613f246440
lelanavilla <cfsfresno@gmail.com>
2018-03-03 22:07:07 -0700

    submitting colorGrid exercise

--- exercises/social/social.js
+++ exercises/social/social.js
@@ -1,11 +1,43 @@
-var classMember ={
-    firstName:"Lelana",
-    lastName:"Villa",
-    age: 37
-    children:[
-        { 
-            name:"destiny",
-            gender: "female",
-            age: 19
+var dinnerParty = {
+    menu: {
+        firstCourse: "salad",
+        mainCourse: "steak",
+        dessert: "ice cream"
+    },
+    guest: [
+         {
+            firstName:"lelana",
+            lastName:" Villa",
+            fullName: function(){
+                return this.firstName.concat(this.lastName);
+            }
+        }
+        ,
+        
+
+         {
+            firstName: "steven",
+            lastName: "Blakley"
+
+        },
+         {
+            firstName: "jen",
+            lastName: "evenson"
+        },
+        {
+                firstName: "kat",
+                lastName:"celis" 
+        
+        }
     ]
-    
+
+,
+    groceryList: [
+        "lettuce", "tomatoes", "dressing", "cucumbers",
+        "steak", "potatoes"
+    ]
+};
+
+console.log(dinnerParty.guest[0].fullName());
+
+
