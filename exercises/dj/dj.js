4575efe50b0cb01acfc53d16d6954a4ec8deb8f7
lelanavilla <cfsfresno@gmail.com>
2018-03-08 10:31:21 -0700

    pushing cuz I havent in a day or so

---
+++ exercises/dj/dj.js
@@ -0,0 +1,22 @@
+var mainBox= document.getElementById("mainBox");
+
+mainBox.addEventListener("mouseover", changeColor);
+mainBox.addEventListener("mousedown", changeColor);
+mainBox.addEventListener("mouseup", changeColor);
+mainBox.addEventListener("scroll", changeColor);
+mainBox.addEventListener("dblclick",changeColor );
+function changeColor(){
+if(event.type === "mouseover"){
+    mainBox.style.backgroundColor = "blue";
+}else if( event.type === "mousedown"){
+    mainBox.style.backgroundColor = "red";
+}else if( event.type === "mouseup"){
+    mainBox.style.backgroundColor = "green";
+}else if( event.type === "scroll"){
+    mainBox.style.backgroundColor = "orange";
+}
+}
+changeColor();
+  
+
+    
---
+++ exercises/travel-form/travelForm.js
@@ -0,0 +1,15 @@
+var fName = document.getElementById("fName");
+var lName = document.getElementById("lName");
+var age = document.getElementById("age");
+var fGender = document.getElementById("fGender");
+var mGender =document.getElementById("mGender");
+var california = document.getElementById("california");
+var paris = document.getElementById("paris");
+var newyork = document.getElementById("newYork";)
+var dietDairy = document.getElementById("dietDairy");
+var dietGluten = document.getElementById("dietGluten");
+var dietPeanut = document.getElementById("dietPeanut");
+
+
+
+}
